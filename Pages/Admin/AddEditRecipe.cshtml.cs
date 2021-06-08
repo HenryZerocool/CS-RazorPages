e08b89e7816f4d8cc26888bfda35fd33235f2828
HenryZerocool <phnguyen7@myseneca.ca>
2021-06-08 14:22:42 -0400

    added simple user authorization

--- Pages/Admin/AddEditRecipe.cshtml.cs
+++ Pages/Admin/AddEditRecipe.cshtml.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CSRazorPages.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CSRazorPages.Pages.Admin
 {
+	[Authorize]
 	public class AddEditRecipeModel : PageModel
 	{
 		private readonly IRecipesService recipesService;
--- Startup.cs
+++ Startup.cs
@@ -7,28 +7,38 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using CSRazorPages.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace CSRazorPages
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-            services.AddTransient<IRecipesService, RecipesService>();
-        }
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            app.UseStaticFiles();
-            app.UseMvcWithDefaultRoute();
-        }
-    }
+			services.AddMvc()
+				.AddRazorPagesOptions(option =>
+				{
+					option.Conventions.AuthorizeFolder("/Admin");
+					option.Conventions.AuthorizeFolder("/Account");
+					option.Conventions.AllowAnonymousToPage("/Account/Login");
+				});
+			services.AddTransient<IRecipesService, RecipesService>();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			app.UseAuthentication();
+			app.UseStaticFiles();
+			app.UseMvcWithDefaultRoute();
+		}
+	}
 }
